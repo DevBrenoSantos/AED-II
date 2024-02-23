9060c9b38b7dc2d991d7182f7029db497b9c16ec
Breno Computador 3º Semestre <c02177@academico.domhelder.edu.br>
2024-02-23 08:18:44 -0300

    Atualizações

--- Lista2_POO/Ex001.java
+++ Lista2_POO/Ex001.java
@@ -1,15 +1,15 @@
 // Breno Amaral Santos - C02177
 package Lista2_POO;
 
-import java.util.Scanner;
 public class Ex001 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        // Instanciando objetos para teste
         Pessoa pessoa1 = new Pessoa("João", "Silva");
         Funcionario pessoa2 = new Funcionario("Nome", "Medeiros", 1750.00);
         Professor pessoa3 = new Professor("José", "Souza", -500.00);
         Professor pessoa4 = new Professor("Ana", "Oliveira", 1500.00);
 
+        // Imprimindo os nomes
         System.out.println("Nomes Completos:");
         System.out.println(pessoa1.getNomeCompleto());
         System.out.println(pessoa2.getNomeCompleto());
@@ -18,12 +18,11 @@ public class Ex001 {
 
         System.out.println("");
 
+        // Imprimindo os salários
         System.out.println("Exibindo os salário:");
         System.out.println(pessoa2.getNome()+ "\n Primeria parcela: " + pessoa2.getSalarioPrimeiraParcela() + "\n Segunda parcela: " + pessoa2.getSalarioSegundaParcela());
         System.out.println(pessoa3.getNome()+ "\n Primeria parcela: " + pessoa3.getSalarioPrimeiraParcela() + "\n Segunda parcela: " + pessoa3.getSalarioSegundaParcela());
         System.out.println(pessoa4.getNome()+ "\n Primeria parcela: " + pessoa4.getSalarioPrimeiraParcela() + "\n Segunda parcela: " + pessoa4.getSalarioSegundaParcela());
-
-        input.close();
     }
 }
 
